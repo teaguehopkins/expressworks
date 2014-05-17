8770fb3735c2f3f20e04fdc14a573aba2125c837
Teague Hopkins <teague@teaguehopkins.com>
2014-05-17 00:32:05 -0400

    Added documentation.

--- jade.js
+++ jade.js
@@ -2,7 +2,8 @@ var express = require('express');
 var app = express();
 
 app.set('view engine', 'jade')
-app.set('views', process.argv[3])
+
+app.set('views', process.argv[3]) //views and path to views, in this case supplied by ExpressWorks
 
 app.get('/home', function(request, response){
   response.render('index', {date:new Date().toDateString()});
