db57ae2f6cdd305935d78559c29de61bcd15eba3
Teague Hopkins <teague@teaguehopkins.com>
2014-05-19 12:04:26 -0400

    Solved Static

---
+++ static.js
@@ -0,0 +1,7 @@
+var express = require('express');
+var path = require('path')
+var app = express();
+
+app.use(express.static(process.argv[3]||path.join(__dirname, 'public'))); //in this case supplied by ExpressWorks
+
+app.listen(process.argv[2]);
